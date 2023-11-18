fe33816c20e37ab6130b0db7e56a08daa953664c
Administrator <moradinamvar@gmail.com>
2023-11-19 01:21:15 +0330

    second commit

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 print("Newwww line")
 print("Newwww line2")
+print("Newwww line3")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
